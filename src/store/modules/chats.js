e216438234bb7a04b6c458bc6186b908c4fd9d75
walimike <walimike139@gmail.com>
2020-03-19 13:14:11 +0300

    modify chat creation process

--- src/store/modules/chats.js
+++ src/store/modules/chats.js
@@ -7,34 +7,63 @@ export default {
     namespaced: true,
     state: {
         loading: false,
-        chartIstance: {},
-        error: ''
+        chatUri: '',
+        members: '',
+        error: '',
+        messages: '',
+        chatMate:''
     },
     mutations: {
         SET_LOADING: (state, payload) => {
             state.loading = payload
         },
         SET_CHAT_INSTANCE: (state, payload) => {
-            state.chartIstance.chatUrl = payload.uri
-            state.chartIstance.members = payload.members
+            state.chatUri = payload.uri
+            state.members = payload.members
         },
         SET_ERROR:  (state, payload) => {
             state.chatUrl = payload
         },
+        SET_CHAT_MESSAGES: (state, payload) =>{
+            state.messages = payload
+        },
+        SET_CHAT_MATE: (state, payload) =>{
+            state.chatMate = payload
+        }
     },
     getters: {
         GET_CHAT_INSTANCE: state => {
-            return state.chartIstance
+            return state.chatUri
+        },
+        GET_CHAT_MESSAGES: state =>{
+            return state.messages
+        },
+        GET_CHAT_MATE: state =>{
+            return state.chatMate
         }
     },
     actions:{
         RETRIEVE_USER_CHAT: async ({commit}, payload) =>{
             commit('SET_LOADING', true)
+            commit('SET_CHAT_MATE', payload)
 
             await axios.post(CHAT_ENDPOINT, payload, {
                 headers: getHeaders()
             }).then(response => {
                 commit('SET_CHAT_INSTANCE', response.data)
+            }).finally(
+                commit('SET_LOADING', false)
+            )
+        },
+        SEND_CHART_MESSAGE: async ({commit}, payload) =>{
+            // Expects {"message":"some message"}, {chartUri:"some characters"}
+            commit('SET_LOADING', true)
+            await axios.post(CHAT_ENDPOINT + payload.chatUri + '/messages/', payload, {
+                headers: getHeaders()
+            }).then(response => {
+                commit('SET_CHAT_MESSAGES', response.data.messages)
+            }).catch(error => {
+                commit(console.log(error.response.data))
             }).finally(
                 commit('SET_LOADING', false)
             )
